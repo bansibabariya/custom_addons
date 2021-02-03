022f96bffd46a7acfa4d1268d24fb5efcf661482
Dharmesh <drchudasama644@gmail.com>
2021-02-03 15:01:28 +0530

    [ADD]code added for changes

--- atharva_theme_base/models/product_template.py
+++ atharva_theme_base/models/product_template.py
@@ -9,3 +9,23 @@ class ProductTemplate(models.Model):
     product_brand_id = fields.Many2one('as.product.brand',string='Brand',help='Select a brand for this product')
     pro_tab_ids = fields.One2many('as.product_tab', 'product_id',string='Product Tabs', help="Set the product tabs")
     category_name = fields.Char("Category Name", translate=True)
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+
+
+class EventInherit(models.Model):
+    _inherit = 'event.event'
+
+    state_ids = fields.Many2many('res.country.state', string='Cities')
+    event_date_ids = fields.One2many(comodel_name="event.date", inverse_name="event_id", string="Event Date", required=False, )
+
+
+
+class EventDate(models.Model):
+    _name = 'event.date'
+
+    date = fields.Date(string="Date")
+    event_id = fields.Many2one(comodel_name="event.event", string="Event", required=False, )
